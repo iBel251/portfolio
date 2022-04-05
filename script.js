318327a1ed75738dc446efa6c888dbcdf6e3b083
iBel251 <ibbimran7@gmail.com>
2022-04-05 11:43:52 +0300

    Fix ESLint error

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ const fullMenu = document.querySelector('.header');
 const navBtns = document.querySelectorAll('.full-menu a');
 
 menubtn.addEventListener('click', () => {
-
   menubtn.classList.toggle('open');
   fullMenu.classList.toggle('open-menu');
-
 });
 
 for (let i = 0; i < 3; i += 1) {
