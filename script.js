802d71209ad8e140c46fa422376b3d14e709ea4c
iBel251 <ibbimran7@gmail.com>
2022-04-07 15:38:36 +0300

    Fixed popup modal and dynamics

--- script.js
+++ script.js
@@ -81,6 +81,17 @@ const projects = [
     demoLink: '',
     srcLink: '#',
   },
+  {
+    id: 8,
+    name: 'Keeping track of hundreds  of components website',
+    name2: 'Multi Post Stories',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    description2: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    imageUrl: 'popup-img.jpg',
+    technologies: ['html', 'Bootstrap', 'Ruby on rails'],
+    demoLink: '',
+    srcLink: '',
+  }
 ];
 
 function renderCards(i) {
@@ -95,11 +106,31 @@ function renderCards(i) {
               ${projects[i].technologies.map((tech) => `<li>${tech}</li>`).join('')}
             </ul>
             </div>
-            <button type="button" onclick="openPopup(${i})" class="cardbtn2">
+            <button type="button" onclick="openPopup(7)" class="cardbtn2">
               See Project
             </button>
           </div>`;
   div.querySelector('.card-new').style.backgroundImage = `url("${projects[i].imageUrl}")`;
+  document.querySelector('.works').appendChild(div);
+}
+
+function renderFirstCard() {
+  const div = document.createElement('div');
+  div.className = 'card1';
+  div.innerHTML = `<img src="gymfit.png" alt="">
+  <h1>Multi-Post Stories</h1>
+  <p>A daily selection of privately personalized reads;
+    no accounts or sign-ups required. has been the industry's
+    standard dummy text ever since the 1500s,
+    when an unknown printer took a standard dummy text.</p>
+  <ul class="card1-btn">
+    <li>css</li>
+    <li>html</li>
+    <li>bootstrap</li>
+    <li>Ruby</li>
+  </ul>
+  <button class="card1btn" onclick="openPopup(7)">See Project</button>
+`;
   document.querySelector('.works').appendChild(div);
 }
 
@@ -112,15 +143,22 @@ function closePopup() {
 function openPopup(i) {
   const target = document.querySelector('.modal-container');
   target.classList.add('active-modal');
-  target.querySelector('.modal-title').textContent = projects[i].name;
-  target.querySelector('.modal-text').textContent = projects[i].description;
+  if(window.innerWidth<600){
+    target.querySelector('.modal-title').textContent = projects[i].name2;
+    target.querySelector('.modal-text').textContent = projects[i].description2;
+  }else{
+    target.querySelector('.modal-title').textContent = projects[i].name;
+    target.querySelector('.modal-text').textContent = projects[i].description;
+  }
+  
+  
   target.querySelector('.modal-img').src = projects[i].imageUrl;
   target.querySelector('.technologies-used-list').innerHTML = projects[i].technologies.map((tech) => `<li>${tech}</li>`).join('');
   target.querySelector('.see-live-btn').href = projects[i].demoLink;
   target.querySelector('.see-src-btn').href = projects[i].srcLink;
   document.body.classList.add('no-scroll-desktop');
 }
-
+renderFirstCard();
 for (let i = 1; i < 7; i += 1) {
   renderCards(i);
 }
